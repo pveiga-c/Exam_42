f205457831b0bd1f0dae3b7bee4eb6dca6b0e2b8
pveiga-c <correiapc@hotmail.com>
2024-10-03 10:48:05 +0100

    cpp00

--- Rank 5/cpp_module00/Warlock.cpp
+++ Rank 5/cpp_module00/Warlock.cpp
@@ -1,27 +1,25 @@
 #include "Warlock.hpp"
 
-Warlock::Warlock(const std::string &name, const std::string &title): _name(name), _title(title)
+Warlock::Warlock(const std::string &name, const std::string &title) : _name(name), _title(title)
 {
-	std::cout << name << ": This looks like another boring day." << std::endl;
+	std::cout << _name << ": This looks like another boring day." << std::endl;
 }
-
 Warlock::~Warlock()
 {
 	std::cout << _name << ": My job here is done!" << std::endl;
 }
 
-
-const std::string &Warlock::getName() const
+const std::string &Warlock::getName () const
 {
 	return (this->_name);
 }
 
-const std::string &Warlock::getTitle() const
+const std::string &Warlock::getTitle () const
 {
 	return (this->_title);
 }
 
-void Warlock::setTitle(const std::string &title)
+void Warlock::setTitle (const std::string &title)
 {
 	this->_title = title;
 }
@@ -30,3 +28,4 @@ void Warlock::introduce() const
 {
 	std::cout << _name << ": I am " << _name << ", " << _title << "!" << std::endl;
 }
+
--- Rank 5/cpp_module00/Warlock.hpp
+++ Rank 5/cpp_module00/Warlock.hpp
@@ -9,17 +9,17 @@ class Warlock
 	private:
 		std::string _name;
 		std::string _title;
+	
 	public:
 		Warlock(const std::string &name, const std::string &title);
 		~Warlock();
 
-		const std::string& getName() const;
-		const std::string& getTitle() const;
-
-		void setTitle(const std::string &title);
+		const std::string& getName () const;
+		const std::string& getTitle () const;
+		void setTitle (const std::string &title);
 
 		void introduce() const;
-
 };
+
 
 #endif
